92c47a92616d21498a76fc6596a57f29e143b94f
Kourosh394 <kouroshfathabadi94@gmail.com>
2026-09-24 11:43:13 +0330

    Add files via upload

--- کلمه 5 حرف حدس زن/shared js.js
+++ کلمه 5 حرف حدس زن/shared js.js
@@ -4,6 +4,29 @@ if (window.location.hostname != "kourosh394.github.io" && window.location.protoc
     alert(`این بازی تقلبی کلمه 5 حرف حدس زن است. لینک تقلبی: ${decodeURIComponent(window.location.href)} لینک اصلی: https://kourosh394.github.io/HTMLCFF/faprgs/کلمه 5 حرف حدس زن/main.html OK را بزنید تا به نسخه ی اصلی بروید.`);
     window.location.href = "https://kourosh394.github.io/HTMLCFF/faprgs/کلمه 5 حرف حدس زن/main.html";
 }
+window.onload = function () {
+    let musicLocation = localStorage.getItem("autoPlayMusic-g");
+    if (musicLocation != null && musicLocation.slice(-4) == ".mp3") {
+        let audio = document.createElement("audio");
+        audio.src = musicLocation;
+        audio.loop = true;
+        document.body.appendChild(audio);
+        let audioPlayButton = document.createElement("button");
+        audioPlayButton.innerHTML = "پخش آهنگ🎵";
+        audioPlayButton.className = "BM";
+        audioPlayButton.onclick = function () {
+            audio.play();
+            audioPlayButton.style.display = "none";
+        };
+        document.body.appendChild(audioPlayButton);
+    }
+};
+function checkLScacheG(LSValue) {
+    if (localStorage.getItem("cache-g") != LSValue) {
+        window.location.href = "main.html";
+        return;
+    }
+}
 function MAW() {
     document.getElementById("money").innerText = `💵: ${money}`;
     localStorage.setItem("money-g", money);
